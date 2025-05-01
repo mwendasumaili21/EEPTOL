cc90f2718c3981bdbca09dc7066ffa5c85cded2c
mwanda <mwendasumaili21@gmail.com>
2025-05-01 21:56:38 +0200

    Initial commit - EEPTOL project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import AppRoutes from "./routes";
+import './index.css';
+
+export default function App() {
+  return (
+    <div className="w-full h-full bg-gray-50">
+      <AppRoutes />
+    </div>
+  );
+}
