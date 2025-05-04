21a8946c0419c7077d432a6e6d44506d40502ab1
mwanda <mwendasumaili21@gmail.com>
2025-05-04 19:38:23 +0200

    Added NavBar and homepage base

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import AppRoutes from "./routes";
+import NavBar from "./components/NavBar";
 import './index.css';
 
 export default function App() {
   return (
     <div className="w-full h-full bg-gray-50">
+      <NavBar />
       <AppRoutes />
     </div>
   );
